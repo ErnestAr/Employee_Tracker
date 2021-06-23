d533faadfc34f7b6bf43332a113ad2e0f0ec4623
ErnestAr <arutiunian.ernest@gmail.com>
2021-06-22 23:30:06 -0400

    call to db functions set up

--- seed.sql
+++ seed.sql
@@ -3,14 +3,14 @@ CREATE DATABASE empoloyees_db;
 
 USE empoloyees_db;
 
-CREATE TABLE department (
+CREATE TABLE departments (
   id INTEGER,
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 
 );
 
-CREATE TABLE  role (
+CREATE TABLE  roles (
   id INTEGER,
   title VARCHAR(30),
   salary    DECIMAL,
@@ -19,7 +19,7 @@ CREATE TABLE  role (
 
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
   id INTEGER ,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
@@ -30,28 +30,28 @@ CREATE TABLE employee (
 );
 
 
-INSERT INTO department (id, name)
+INSERT INTO departments (id, name)
 VALUES (5, 'marketing');
-INSERT INTO department (id, name)
+INSERT INTO departments (id, name)
 VALUES (3, 'service');
-INSERT INTO department (id, name)
+INSERT INTO departments (id, name)
 VALUES (4, 'finance');
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (24, manager, 60,000, 3);
-INSERT INTO role (id, title, salary, department_id)
-VALUES (25, finance advisor, 65,000, 4);
-INSERT INTO role (id, title, salary, department_id)
-VALUES (26, District manager, 60,000, 3);
-INSERT INTO role (id, title, salary, department_id)
-VALUES (27, manager, 60,000, );
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (24, "manager", 60000, 3);
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (25, "finance advisor", 65000, 4);
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (26, "District manager", 60000, 3);
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (27, "manager", 60000, 5 );
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (234, Will, Smith, 26, 12);
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (235, Alex, Courtie, 26, 13);
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (236, Nick, Lew, 26, 14);
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (237, Mr, Anderson, 26, 15);
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+VALUES (234, "Will", "Smith", 26, 12);
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+VALUES (235, "Alex", "Courtie", 26, 13);
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+VALUES (236, "Nick", "Lew", 26, 14);
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+VALUES (237, "Mr", "Anderson", 26, 15);
 
--- tracker.js
+++ tracker.js
@@ -18,14 +18,54 @@ connection.connect((err) => {
 
 
  function Add() {
-    this.departments = () => {
+    this.department = (id, name) => {
+        console.log('Inserting a new department...\n');
+        const query = connection.query(
+          'INSERT INTO departments SET ?',
+          {
+            id: id,
+            name: name
+          },
+          (err, res) => {
+            if (err) throw err;
+            console.log(`${res.affectedRows} department inserted!\n`);
+          }
+        );
 
     }
-    this.roles = () => {
+    this.role = (id, title, salary, department_id) => {
+        console.log('Inserting a new role...\n');
+        const query = connection.query(
+          'INSERT INTO roles SET ?',
+          {
+            id: id,
+            title: title,
+            salary:salary,
+            department_id: department_id,
 
+          },
+          (err, res) => {
+            if (err) throw err;
+            console.log(`${res.affectedRows} role inserted!\n`);
+          }
+        );
     }
-    this.employee = () => {
-
+    this.employee = (id, first_name, last_name, role_id, manager_id) => {
+        console.log('Inserting a new employee...\n');
+        const query = connection.query(
+          'INSERT INTO employees SET ?',
+          {
+            id: id,
+             first_name: first_name,
+              last_name: last_name, 
+              role_id: role_id,
+              manager_id: manager_id
+          },
+          (err, res) => {
+            if (err) throw err;
+            console.log(`${res.affectedRows} employee inserted!\n`);
+          }
+        );
     }
 }
 
@@ -33,20 +73,43 @@ function View() {
     this.departments = () => {
         connection.query('SELECT * FROM departments', (err, res) => {
             if (err) throw err;
-            console.log(res);
+            console.table(res);
             connection.end();
           });
     }
     this.roles = () => {
-
+        connection.query('SELECT * FROM departments', (err, res) => {
+            if (err) throw err;
+            console.table(res);
+            connection.end();
+          });
     }
     this.employee = () => {
-
+        connection.query('SELECT * FROM employee', (err, res) => {
+            if (err) throw err;
+            console.table(res);
+            connection.end();
+          });
     }
 }
 
 function Update() {
-    this.employee_roles = () => {
+    this.employee_roles = (input_name) => {
+        console.log('Updating selected employee role...\n');
+        const query = connection.query(
+          'UPDATE employees SET ? WHERE ?',
+          [
+            {
+              first_name: input_name,
+            },
+          ],
+          (err, res) => {
+            if (err) throw err;
+            console.log(`${res.affectedRows} employee updated!\n`);
+          }
+        )
     }
   }
+
+
 
